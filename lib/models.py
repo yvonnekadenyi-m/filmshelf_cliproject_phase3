4ae99093b585f6cccdd833cc4447c04a2062f464
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-04 00:32:07 +0300

    added to models.py

--- lib/models.py
+++ lib/models.py
@@ -3,3 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
 Base = declarative_base()
+
+engine = create_engine('sqlite:///example.db')
+
