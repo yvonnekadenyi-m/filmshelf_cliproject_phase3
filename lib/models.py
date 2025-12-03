2963788a062262c22c3a1bc5c35936a20d64cc39
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-04 00:22:35 +0300

    added to folder

---
+++ lib/models.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
