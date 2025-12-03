283afb630e5ea6a4084da24f21646a728e36181a
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-04 00:30:38 +0300

    added to models.py

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
